cb459c6508c6c09e31e522f316627c56f2e56743
chesterosoronyas <31725146+chesterosoronyas@users.noreply.github.com>
2022-08-29 11:22:39 +0300

    Validation of the county combobox and its attributes

--- cypress/e2e/Filters/TestScripts.cy.js
+++ cypress/e2e/Filters/TestScripts.cy.js
@@ -1,17 +1,25 @@
 describe("DWHTest", function () {
   // test case
-  it("Test Case2", function () {
+  it("Select NAIROBI county and validate its attribute", function () {
     cy.visit("https://data.kenyahmis.org:9000/#/");
+    cy.get("#county").click({ force: true });
 
-    // checking by values
-    cy.get('input[type="checkbox"]').check([
-      "Manual Tester",
-      "Automation Tester",
-    ]);
-    // selecting a value from static dropdown
-    cy.get('select[name="County"]')
-      .select("Nairobi")
-      // asserting the option selected
-      .should("have.text", "Nairobi");
+    cy.xpath('//*[@id="county"]/input').click({ force: true, multiple: true });
+
+    //selects NAIROBI
+    cy.xpath('//*[@id="county"]/div[2]/div[25]/span').click({
+      force: true,
+      multiple: true,
+    });
+
+
+    cy.xpath('//*[@id="county"]/a').eq(1).should('have.text','NAIROBI');
+
+    //remove Nairobi
+    //cy.xpath('//*[@id="county"]/a/i').click({force:true,multiple:true})
+
+    // cy.get('#county > a').type("Nairobi")
+    // asserting the option selected
+    // .should("have.text", "Nairobi");
   });
 });
