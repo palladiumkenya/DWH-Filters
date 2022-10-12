62a58884b26626584a8c0cf74ea43bdc364e9f97
Wambui P <peris>
2022-10-12 15:50:09 +0300

    Adding HTSUptake testcases

---
+++ cypress/e2e/Filters/HTSUptake.cy.js
@@ -0,0 +1,125 @@
+describe("DWHCT", function () {
+    // test case
+    it("Select KISUMU county and validate its attribute", function () {
+      cy.visit("https://dwh.nascop.org/#/hiv-testing/uptake");
+      cy.get("#county").click({ force: true });
+  
+      cy.xpath('//*[@id="county"]/input').click({ force: true, multiple: true });
+  
+      //selects NAIROBI
+      cy.xpath('//*[@id="county"]/div[2]/div[17]/span').click({
+        force: true,
+        multiple: true,
+      });
+       //cy.get(':nth-child(4) > :nth-child(1) > .form-group > #county').click()
+       //.contains('Nairobi').click()
+       //cy.get().contains('have text'Nairobi')
+  
+      cy.xpath('//*[@id="county"]/a').eq(1).should('have.text','KISUMU');
+  
+      //remove Nairobi
+      //cy.xpath('//*[@id="county"]/a/i').click({force:true,multiple:true})
+  
+      // cy.get('#county > a').type("Nairobi")
+      // asserting the option selected
+      // .should("have.text", "Nairobi");
+    });
+  
+  
+    //it("Select KASARANI sub-county and validate its attribute", function () {
+    //  cy.get("#subCounty").click({ force: true });
+     // cy.xpath('//*[@id="subCounty"]/input').click({
+     //   force: true,
+       // multiple: true,
+      //});
+      //selects KASARANI
+      //cy.get(':nth-child(4) > :nth-child(1) > .form-group > #county').should('include.text', 'Kasarani');
+  
+      it("Select KISUMU CENTRAL sub-county and validate its attribute", function () {
+        cy.get("#subCounty").click({ force: true });
+      
+        cy.xpath('//*[@id="subCounty"]/input').click({force: true,multiple: true,});
+        //selects KISUMU CENTRAL
+      
+      
+        //cy.get(':nth-child(4) > :nth-child(1) > .form-group > #sub-county').should('include.text', 'Kasarani');
+    
+     
+      cy.xpath('//*[@id="subCounty"]/div[2]/div[1]/span').click({
+        force: true,
+        multiple: true,
+     });
+      cy.xpath('//*[@id="subCounty"]/a').eq(1).should("have.text", "KISUMU CENTRAL");
+      //remove KASARANI
+      //cy.xpath('//*[@id="sub-county"]/a/i').click({force:true,multiple:true})
+      // cy.get('#sub-county > a').type("Kasarani")
+      // asserting the option selected
+      // .should("have.text", "Kasarani");
+    });
+  
+    it("Select Kisumu County Hospital facility and validate its attribute", function () {
+        cy.get("#facility").click({ force: true });
+       
+        cy.xpath('//*[@id="facility"]/input').click({ force: true, multiple: true });
+
+        cy.xpath('//*[@id="facility"]/div[2]/div[5]/span').click({
+       force: true,
+       multiple: true,
+       });
+  
+  
+      cy.xpath('//*[@id="facility"]/a').should('include.text','Kisumu County Hospital' );
+  
+      //remove USAID Fahari ya Jamii
+      //cy.xpath('//*[@id="facility"]/a/i').click({force:true,multiple:true})
+  
+      // cy.get('#partner > a').type("USAID Fahari ya Jamii")
+      // asserting the option selected
+      // .should("have.text", "USAID Fahari ya Jamii");
+    });
+
+       
+    it("Select CIHEB ENTRENCH partner and validate its attribute", function () {
+      cy.get("#partner").click({ force: true });
+     
+      cy.xpath('//*[@id="partner"]/input').click({ force: true, multiple: true });
+  
+      //selects CIHEB CONNECT
+      //cy.get('.visible > :nth-child(5) > .text').click({
+        //force:true,
+       // multiple:true
+      //});
+       cy.xpath('//*[@id="partner"]/div[2]/div[1]/span').click({
+       force: true,
+       multiple: true,
+       });
+  
+  
+      cy.xpath('//*[@id="partner"]/a').should('include.text','CIHEB ENTRENCH' );
+  
+      //remove USAID Fahari ya Jamii
+      //cy.xpath('//*[@id="facility"]/a/i').click({force:true,multiple:true})
+  
+      // cy.get('#partner > a').type("USAID Fahari ya Jamii")
+      // asserting the option selected
+      // .should("have.text", "USAID Fahari ya Jamii");
+    });
+  
+    it("Select No Agency agency and validate its attribute", function () {
+      cy.get("#agency").click({ force: true });
+     
+      cy.xpath('//*[@id="agency"]/input').click({ force: true, multiple: true });
+  
+      //selects ahf
+      cy.xpath('//*[@id="agency"]/div[2]/div/span').click({
+        force: true,
+        multiple: true,
+    })
+        cy.get(':nth-child(4) > :nth-child(5) > .form-group > .field > .ui > input').then(elem=>{
+   
+            elem.val('Jun 2022 - Jun 2022')
+    
+          })
+     });
+
+  })
