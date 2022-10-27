574db960fce2f2a3ff9361a141d0338ce6af221c
Wambui P <peris>
2022-10-27 10:11:26 +0300

    Adding Vluptake test cases

---
+++ cypress/e2e/Filters/NairobiCounty/ReportingratesDagorettiNorth.cy.js
@@ -0,0 +1,121 @@
+describe("DWHCT", function () {
+    // test case
+    it("Select NAIROBI county and validate its attribute", function () {
+      cy.visit("https://dwh.nascop.org/#/reporting-rates/ct");
+      cy.get("#county").click({ force: true });
+  
+      cy.xpath('//*[@id="county"]/input').click({ force: true, multiple: true });
+  
+      //selects NAIROBI
+      cy.xpath('//*[@id="county"]/div[2]/div[26]/span').click({
+        force: true,
+        multiple: true,
+      });
+  
+      cy.xpath('//*[@id="county"]/a').eq(1).should('have.text','NAIROBI');
+  
+      
+    });
+  
+      it("Select KASARANI sub-county and validate its attribute", function () {
+        cy.get("#subCounty").click({ force: true });
+      
+        cy.xpath('//*[@id="subCounty"]/input').click({force: true,multiple: true,});
+        //selects KASARANI
+      
+      
+        //cy.get(':nth-child(4) > :nth-child(1) > .form-group > #sub-county').should('include.text', 'Kasarani');
+    
+     
+      cy.xpath('//*[@id="subCounty"]/div[2]/div[10]/span').click({
+        force: true,
+        multiple: true,
+     });
+      cy.xpath('//*[@id="subCounty"]/a').eq(1).should("have.text", "KASARANI");
+      //remove KASARANI
+      //cy.xpath('//*[@id="sub-county"]/a/i').click({force:true,multiple:true})
+      // cy.get('#sub-county > a').type("Kasarani")
+      // asserting the option selected
+      // .should("have.text", "Kasarani");
+    });
+  
+  
+    it("Select USAID Fahari ya Jamii partner and validate its attribute", function () {
+      cy.get("#partner").click({ force: true });
+     
+      cy.xpath('//*[@id="partner"]/input').click({ force: true, multiple: true });
+  
+      //selects USAID Fahari ya Jamii
+      //cy.get('.visible > :nth-child(5) > .text').click({
+        //force:true,
+       // multiple:true
+      //});
+       cy.xpath('//*[@id="partner"]/div[2]/div[4]/span').click({
+       force: true,
+       multiple: true,
+       });
+  
+  
+      cy.xpath('//*[@id="partner"]/a').should('include.text','USAID Fahari ya Jamii' );
+  
+      //remove USAID Fahari ya Jamii
+      //cy.xpath('//*[@id="facility"]/a/i').click({force:true,multiple:true})
+  
+      // cy.get('#partner > a').type("USAID Fahari ya Jamii")
+      // asserting the option selected
+      // .should("have.text", "USAID Fahari ya Jamii");
+    });
+  
+    it("Select AHF agency and validate its attribute", function () {
+      cy.get("#agency").click({ force: true });
+     
+      cy.xpath('//*[@id="agency"]/input').click({ force: true, multiple: true });
+  
+      //selects ahf
+      cy.xpath('//*[@id="agency"]/div[2]/div[1]/span').click({
+        force: true,
+        multiple: true,
+     });
+     cy.get(':nth-child(4) > :nth-child(5) > .form-group > .field > .ui > input').then(elem=>{
+   
+      elem.val('Jun 2022 - Jun 2022')
+  
+
+    // it("Select 'period' and validate its attribute", function () {
+        //cy.get("#period").click({ force: true });
+
+        //cy.xpath('//*[@id="period"]/input').click({ force: true, multiple: true });
+
+        //selects period
+        //cy.xpath('//*[@id="period"]/div[3]/div[5]/span').click({
+        //force: true,
+       // multiple: true,
+   // });
+
+   //cy.get(':nth-child(4) > :nth-child(5) > .form-group > .field > .ui > input').then(elem=>{
+   
+    //elem.val('Jun 2022 - Jun 2022')
+
+  //})
+    
+    })
+     // cy.get(':nth-child(4) > :nth-child(4) > .form-group > #agency').should('include.text','AHF');
+  
+      //remove AHF
+      //cy.xpath('//*[@id="agency"]/a/i').click({force:true,multiple:true})
+  
+      // cy.get('#agency > a').type("AHF")
+      // asserting the option selected
+      // .should("have.text", "AHF");
+    });
+  
+    it("Input the reporting rate period", function(){
+     // cy.get(':nth-child(4) > :nth-child(5) > .form-group > .field > .ui > input').click({force:true});
+      cy.get(':nth-child(4) > :nth-child(5) > .form-group > .field > .ui > input').then(elem=>{
+        elem.val('Jun 2022 - Jun 2022')
+      })
+      
+    })
+    
+    
+  })
--- cypress/e2e/Filters/adverseadults.cy.js
+++ cypress/e2e/Filters/adverseadults.cy.js
@@ -1,13 +1,13 @@
 describe("DWHCT", function () {
     // test case
     it("Select NAIROBI county and validate its attribute", function () {
-      cy.visit("https://dwh.nascop.org/#/hiv-testing/prep");
+      cy.visit("https://dwh.nascop.org/#/hiv-treatment/adverseEvent/adults");
       cy.get("#county").click({ force: true });
   
       cy.xpath('//*[@id="county"]/input').click({ force: true, multiple: true });
   
       //selects NAIROBI
-      cy.xpath('//*[@id="county"]/div[2]/div[29]/span').click({
+      cy.xpath('//*[@id="county"]/div[2]/div[25]/span').click({
         force: true,
         multiple: true,
       });
@@ -39,7 +39,7 @@ describe("DWHCT", function () {
         cy.get("#subCounty").click({ force: true });
       
         cy.xpath('//*[@id="subCounty"]/input').click({force: true,multiple: true,});
-        //selects WESTLANDS
+        //selects KASARANI
       
       
         //cy.get(':nth-child(4) > :nth-child(1) > .form-group > #sub-county').should('include.text', 'Kasarani');
@@ -57,36 +57,36 @@ describe("DWHCT", function () {
       // .should("have.text", "Kasarani");
     });
 
-    //it("Select Kasarani Health Centre facility and validate its attribute", function () {
-       // cy.get("#facility").click({ force: true });
+    it("Select Riruta Health Centre facility and validate its attribute", function () {
+        cy.get("#facility").click({ force: true });
+       
+        cy.xpath('//*[@id="facility"]/input').click({ force: true, multiple: true });
     
-       // cy.xpath('//*[@id="facility"]/input').click({
-        //  force: true,
-        //  multiple: true,
-       // });
+        //selects USAID Fahari ya Jamii
+        //cy.get('.visible > :nth-child(5) > .text').click({
+          //force:true,
+         // multiple:true
+        //});
+         cy.xpath('//*[@id="facility"]/div[2]/div[6]/span').click({
+         force: true,
+         multiple: true,
+         });
     
-        //selects Kasarani Health Centre
-       // cy.xpath('//*[@id="facility"]/div[2]/div[3]/span').click({
-         // force: true,
-          //multiple: true,
-       // });
     
-       // cy.xpath('//*[@id="facility"]/a')
-          //.eq(1)
-   //      // .should("have.text", "Kasarani Health Centre");
-    //})
+        cy.xpath('//*[@id="facility"]/a').should('include.text','Riruta Health Centre' );
   
+    });
     it("Select CIHEB CONNECT partner and validate its attribute", function () {
       cy.get("#partner").click({ force: true });
      
       cy.xpath('//*[@id="partner"]/input').click({ force: true, multiple: true });
   
-      //selects CIHEB CONNECT
+      //selects USAID Fahari ya Jamii
       //cy.get('.visible > :nth-child(5) > .text').click({
         //force:true,
        // multiple:true
       //});
-       cy.xpath('//*[@id="partner"]/div[2]/div[1]/span').click({
+       cy.xpath('//*[@id="partner"]/div[2]/div[2]/span').click({
        force: true,
        multiple: true,
        });
@@ -102,38 +102,61 @@ describe("DWHCT", function () {
       // .should("have.text", "USAID Fahari ya Jamii");
     });
   
-    it("Select No Agency agency and validate its attribute", function () {
+    it("Select AHF agency and validate its attribute", function () {
       cy.get("#agency").click({ force: true });
      
       cy.xpath('//*[@id="agency"]/input').click({ force: true, multiple: true });
   
       //selects ahf
-      cy.xpath('//*[@id="agency"]/div[2]/div/span').click({
+      cy.xpath('//*[@id="agency"]/div[2]/div[1]/span').click({
         force: true,
         multiple: true,
-    });
-        cy.get('.animated > :nth-child(1) > :nth-child(6) > .form-group > .field > .ui > input').then(elem=>{
-            elem.val('Jun 2022 - Jun 2022')
-          })
-     
-
-    // it("Input the reporting rate period", function(){
-
-        // cy.get(':nth-child(4) > :nth-child(5) > .form-group > .field > .ui > input').click({force:true});
+     });
+     cy.xpath('//*[@id="agency"]/a').should('include.text','AHF' );
    
-        // cy.get(':nth-child(4) > :nth-child(5) > .form-group > .field > .ui > input').then(elem=>{
-          // elem.val('Jun 2022 - Jun 2022')
-        // })
-      // })
-
+     //cy.get(':nth-child(4) > :nth-child(6) > .form-group > .field > .ui > input').then(elem=>{
+   
+        //elem.val('Jun 2022 - Jun 2022')
+   // });
+});
+  
+    it("Select Female gender and validate its attribute", function () {
+        cy.get("#gender").click({ force: true });
     
-     // cy.get(':nth-child(4) > :nth-child(4) > .form-group > #agency').should('include.text','AHF');
-  
-      //remove AHF
-      //cy.xpath('//*[@id="agency"]/a/i').click({force:true,multiple:true})
-  
-      // cy.get('#agency > a').type("AHF")
-      // asserting the option selected
-      // .should("have.text", "AHF");
-    })
+        cy.xpath('//*[@id="gender"]/input').click({ force: true, multiple: true });
+    
+        //selects CDC
+        cy.xpath('//*[@id="gender"]/div[2]/div[2]').click({
+          force: true,
+          multiple: true,
+        });
+    
+        cy.xpath('//*[@id="gender"]/a').eq(1).should("have.text", "Female");
+    
+    });
+
+    it("Select 20-24 age group and validate its attribute", function () {
+        cy.get("#datimAgeGroup").click({ force: true });
+    
+        cy.xpath('//*[@id="datimAgeGroup"]/input').click({
+          force: true,
+          multiple: true,
+        });
+    
+        //selects CDC
+    
+        cy.xpath('//*[@id="datimAgeGroup"]/div[2]/div[6]').click({
+          force: true,
+    
+          multiple: true,
+        });
+    
+        cy.xpath('//*[@id="datimAgeGroup"]/a')
+          .eq(1)
+          .should("have.text", "20 to 24");
+    
+       
+      });
+
+      
   })
